4d605a9815203d9693e1da129ed453edea876f20
Shaheer Imam <42369778+Shaheer-Imam@users.noreply.github.com>
2020-05-16 22:09:13 +0500

    line detection scripts added

---
+++ Open CV/line detection.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('./images/soduku.jpg')
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 100, 170, apertureSize = 3)
+
+cv2.imshow("edges", edges)
+cv2.waitKey(0)
+
+lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+
+for line in lines:
+    rho, theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
+    cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+cv2.imshow('Hough Lines', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
